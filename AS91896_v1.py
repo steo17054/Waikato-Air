05e1692c27b2f799f2e5f514d2b19a36d38f3e6c
Ocean Steer <steo17054@wainuiomatahigh.school.nz>
2020-08-20 23:34:12 +0000

    before testing classes

--- AS91896_v1.py
+++ AS91896_v1.py
@@ -1,6 +1,8 @@
+#loop to restart the programme if the user wants to enter for another user
 restart_loop = True
 while restart_loop == True:
-  num_seats_per_plane = 130
+  #constant variables for calculating discouts on fares
+  num_seats = 130
   wellington_regular_fare = 70
   auckland_regular_fare = 80
   rotorua_regular_fare = 50
@@ -12,6 +14,7 @@ while restart_loop == True:
   def error():
     print("ERROR: Please type a valid input: ")
 
+  #creating a defintion for restarting the programe if the user wants to enter for another discount
   def restart():
     restart_bool_loop = True
     while restart_bool_loop == True:
@@ -28,42 +31,45 @@ while restart_loop == True:
         restart_bool_loop = True 
 
   #class defintion to gain all of the user inputs needed for discount calculations
-  def user_inputs():
+  class user_inputs(destination, next_day):
   #code to get the destination input from the user and to error catch invalid inputs
-    destination_loop = True
-    while destination_loop == True:
-      destination = str(input("Firstly, where are you looking to fly to out of our available destinations. (Type: Auckland, Wellington or Rotorua): "))
-      destination = destination.upper()
-      if destination == "AUCKLAND":
-          destination_loop = False
-      elif destination == "WELLINGTON":
-          destination_loop = False
-      elif destination == "ROTORUA":
-          destination_loop = False
-      else:
-          error()
-          destination_loop = True
+    def destinaiton_input():
+      destination_loop = True
+      while destination_loop == True:
+        destination = str(input("Firstly, where are you looking to fly to out of our available destinations. (Type: Auckland, Wellington or Rotorua): "))
+        destination = destination.upper()
+        global destination
+        if destination == "AUCKLAND":
+            destination_loop = False
+        elif destination == "WELLINGTON":
+            destination_loop = False
+        elif destination == "ROTORUA":
+            destination_loop = False
+        else:
+            error()
+            destination_loop = True
   #code to get the input on whether the user can travel tommorow or not and to error catch invalid inputs
-    next_day_loop = True
-    while next_day_loop == True:
-      next_day = str(input("Would you be able to travel to your destination tommorow (Type 'YES' or 'NO'): "))
-      next_day = next_day.upper()
-      if next_day == "YES":
-        next_day_loop = False
-      elif next_day == "NO":
-        print("Sorry there are no discounts we can offer you if you can't travel tommrow")
-        restart()
-        next_day_loop = False
-      else:
-        error()
-        next_day_loop = True
+    def next_day_input():
+      next_day_loop = True
+      while next_day_loop == True:
+        next_day = str(input("Would you be able to travel to your destination tommorow (Type 'YES' or 'NO'): "))
+        next_day = next_day.upper()
+        if next_day == "YES":
+          next_day_loop = False
+        elif next_day == "NO":
+          print("Sorry there are no discounts we can offer you if you can't travel tommorow")
+          restart()
+          next_day_loop = False
+        else:
+          error()
+          next_day_loop = True
 
-    return destination, next_day
+      return destination, next_day
 
   #class defintion to calculate all the discounts for the users fare
-  def user_calculations(destination, next_day):
+  def user_calculations(destination, next_day, num_seats, discount, percentage_discount, discount_fare):
     if next_day == "YES":
-      num_seats = num_seats_per_plane - 1
+      num_seats = num_seats - 1
       discount = num_seats / 5
       percentage_discount = discount / 100
       if destination == "AUCKLAND":
@@ -74,9 +80,11 @@ while restart_loop == True:
         discount_fare = rotorua_regular_fare - discount
     return num_seats, discount, percentage_discount, discount_fare 
 
+  #creating defintion to form the paragraph for the user to present their discounts
   def discount_paragraph(destinaiton, num_seats, discount, percentage_discount, discount_fare):
     print("Hello, today we are able to offer you a {}% discount on your flight tommorow to {}. As there was {} seats left on the plane we are able to offer you the ... discount this being a discount of ${}, bringing the total cost of your flight to {} down to ${}. Thank you for working with Waikato Air and we look foward to seeing you tommorow for your flight" .format(percentage_discount, destinaiton, num_seats, discount, destinaiton, discount_fare))
 
+  #running the defintions in the code
   user_inputs(destination, next_day)
   user_calculations(destination, next_day)
   discount_paragraph(destinaiton, num_seats, discount, percentage_discount, discount_fare)            
